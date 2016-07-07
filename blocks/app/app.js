e4f6a4b39189382a1b511f1eff2c9ec328f4daac
Nicolai Voronin <naranjadito@gmail.com>
2016-07-07 10:49:11 +0300

    Update project

--- blocks/app/app.js
+++ blocks/app/app.js
@@ -1,43 +1,52 @@
 (function () {
-	'use strict';
+	"use strict";
 	let Menu = window.Menu;
-
+	let Form = window.Form;
 	window.menu = new Menu({
-		el: document.querySelector('.js-menu'),
+		el: document.querySelector(".js-menu"),
 		data: {
-			title: 'SINGLE PAGE APPLICATION',
+			title: "SINGLE PAGE APPLICATION",
 			items:  [
 				{
-					href: 'https://vk.com/',
-					anchor: 'vk.com'
+					href: "https://vk.com/",
+					anchor: "vk.com"
 				},
 				{
-					href: 'https://ok.ru/',
-					anchor: 'ok.ru'
+					href: "https://ok.ru/",
+					anchor: "ok.ru"
 				},
 				{
-					href: 'https://yahoo.com/',
-					anchor: 'yahoo.com'
+					href: "https://yahoo.com/",
+					anchor: "yahoo.com"
 				},
 				{
-					href: 'https://vk.com/',
-					anchor: 'vk.com'
+					href: "https://vk.com/",
+					anchor: "vk.com"
 				},
 				{
-					href: 'https://ok.ru/',
-					anchor: 'ok.ru'
+					href: "https://ok.ru/",
+					anchor: "ok.ru"
 				},
 				{
-					href: 'https://yahoo.com/',
-					anchor: 'yahoo.com'
+					href: "https://yahoo.com/",
+					anchor: "yahoo.com"
 				}
 			]
 		}
 	});
+	window.form = new Form({
+		el: document.querySelector(".js-form"),
+		action: "menu.pickItem();"
+	});
+	// pickItem(item) {
+	// 	this.trigger("pick", {
+	// 		href: item.getAttribute("href"),
+	// 		anchor: item.textContent
+	// 	});
+	// }
 
+	menu.el.addEventListener("pick", function () {
 
-	menu.el.addEventListener('pick', function () {
-		debugger;
 	});
 
 })();
